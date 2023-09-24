9b3f382cd0dc049f9f87141ca364d5499e6e8dbe
赵紫菜 <25471437+13584452567@users.noreply.github.com>
2023-09-24 12:55:58 +0800

    Update time.cpp

--- time.cpp
+++ time.cpp
@@ -1,14 +1,15 @@
-﻿ #include <string>
- #include <time.h>
+﻿#define _CRT_SECURE_NO_WARNINGS   
+#include <string>
+#include <time.h>
 #include <iostream> 
  using namespace std;
-     
+ 
     string getTime()
     {
         time_t timep;
         time (&timep);
         char tmp[64];
-        strftime(tmp, sizeof(tmp), "%Y.%m.%d_%H:%M:%S",localtime(&timep) );
+        strftime(tmp, sizeof(tmp), "%Y.%m.%d_%H.%M.%S",localtime(&timep) );
         return tmp;
     }
      
